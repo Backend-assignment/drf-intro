bd23fd3a3466710bb849bdfff5e00465a9a125dd
ZARIFMAC <uzbzarif@gmail.com>
2023-04-06 10:04:34 +0500

    serializers

--- api/views.py
+++ api/views.py
@@ -4,20 +4,17 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .models import Todo
-
+from .serializers import TodoSerializer
 class TodoView(APIView):
     def get(self, request: Request) -> Response:
         todos = Todo.objects.all()
+        print(type(todos[0]))
         data = {
             "results": []
         }
+        serializer = TodoSerializer(todos[0])
+        
         for todo in todos:
-            data["results"].append({
-                "task": todo.task,
-                "description": todo.description,
-                "completed": todo.completed,
-                "created_at": todo.created_at,
-                "updated_at": todo.updated_at
-            })
+            data["results"].append(serializer.data)
 
         return Response(data, status=status.HTTP_200_OK)
